728bff4456b341c5f2f906d193c6d1055dcbee39
GabrielMayrand <gabmayrand@hotmail.ca>
2026-01-30 16:06:57 -0500

    test: dotnet dap

--- config/lang/cs.nix
+++ config/lang/cs.nix
@@ -1,24 +1,39 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  ...
+}:
 {
   plugins.lsp.servers.roslyn_ls.enable = true;
-  extraPlugins = [
-    {
-      plugin = (
-        pkgs.vimUtils.buildVimPlugin {
-          name = "dap-cs";
-          src = pkgs.fetchFromGitHub {
-            owner = "NicholasMata";
-            repo = "nvim-dap-cs";
-            rev = "main";
-            hash = "sha256-htcBYnT/ScFxe/TKtXJMmDjEz2I3ZEDgud7jEsOqsNE=";
-          };
-        }
-      );
-      config = ''lua require("dap-cs").setup() '';
-
-    }
+  extraPackages = with pkgs; [
+    netcoredbg
   ];
-  plugins.neotest.adapters = {
-    dotnet.enable = true;
-  };
+
+  extraConfigLua = ''
+        local dap = require("dap")
+
+    local dap = require("dap")
+    local dap_utils = require("dap.utils")
+
+    dap.adapters.coreclr = {
+      type = "executable",
+      command = "netcoredbg",
+      args = { "--interpreter=vscode" },
+    }
+
+    dap.configurations.cs = {
+      {
+        type = "coreclr",
+        name = "Attach to running .NET (smart)",
+        request = "attach",
+        processId = function()
+          return dap_utils.pick_process({
+            filter = function(proc)
+              return proc.name:match("dotnet")
+            end,
+          })
+        end,
+      },
+    }
+  '';
+
 }

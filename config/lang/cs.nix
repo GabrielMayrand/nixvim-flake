3cc68e409babe43783c1f9e0fb891623ed35dd97
Gabriel Mayrand <gabm@premiertech.com>
2026-02-03 16:02:09 -0500

    test: dotnet dap

--- config/lang/cs.nix
+++ config/lang/cs.nix
@@ -4,13 +4,27 @@
 }:
 {
   plugins.lsp.servers.roslyn_ls.enable = true;
+  
+  # Enable easy-dotnet plugin for better .NET workflow
+  plugins.easy-dotnet = {
+    enable = true;
+    settings = {
+      # Auto bootstrap namespace when creating new files
+      auto_bootstrap_namespace = true;
+      
+      # Test runner configuration
+      test_runner = {
+        enable_buffer_test_execution = true;
+        viewmode = "float";
+      };
+    };
+  };
+  
   extraPackages = with pkgs; [
     netcoredbg
   ];
 
   extraConfigLua = ''
-        local dap = require("dap")
-
     local dap = require("dap")
     local dap_utils = require("dap.utils")
 
@@ -21,6 +35,15 @@
     }
 
     dap.configurations.cs = {
+      {
+        type = "coreclr",
+        name = "Launch .NET",
+        request = "launch",
+        program = function()
+          -- This will use easy-dotnet's project picker if available
+          return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/bin/Debug/", "file")
+        end,
+      },
       {
         type = "coreclr",
         name = "Attach to running .NET (smart)",

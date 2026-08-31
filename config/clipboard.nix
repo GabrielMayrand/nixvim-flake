0bb2a10a148df9216dad2e635d098b0f42ce34c9
Gabriel Mayrand <gabm@premiertech.com>
2026-08-31 14:12:57 -0400

    fix: clipboard when using tmux

--- config/clipboard.nix
+++ config/clipboard.nix
@@ -5,11 +5,12 @@
   clipboard.providers.xclip.enable = true;
 
   extraConfigLua = ''
-    -- Enable OSC 52 clipboard over SSH or when no graphical display server is detected
+    -- Enable OSC 52 clipboard over SSH, inside Tmux, or when no graphical display server is detected
     local is_ssh = vim.env.SSH_CONNECTION ~= nil or vim.env.SSH_CLIENT ~= nil or vim.env.SSH_TTY ~= nil
+    local is_tmux = vim.env.TMUX ~= nil
     local has_display = vim.env.DISPLAY ~= nil or vim.env.WAYLAND_DISPLAY ~= nil
 
-    if is_ssh or not has_display then
+    if is_ssh or is_tmux or not has_display then
       vim.g.clipboard = {
         name = "OSC 52",
         copy = {

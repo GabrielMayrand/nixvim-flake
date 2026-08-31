fe9c4db406c8dc5d2de68cf19b6e57de3e963f2f
Gabriel Mayrand <gabm@premiertech.com>
2026-08-31 09:41:09 -0400

    fix: date in git blame

--- config/git.nix
+++ config/git.nix
@@ -4,6 +4,7 @@
   plugins.gitblame = {
     enable = true;
     settings.enable = false;
+    settings.date_format = "%r";
   };
   plugins.diffview.enable = true;
   # plugins.git-conflict.enable = true;
